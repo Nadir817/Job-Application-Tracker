54a6905fe8356223c456d587ea93dd6eb7fe8dc5
Nadir <nadir.erkin@gmail.com>
2023-03-20 10:40:05 -0400

    Key functionality except user login is finished

--- frontend/src/components/JobLists.jsx
+++ frontend/src/components/JobLists.jsx
@@ -6,18 +6,30 @@ const ListsContext = createContext({
 })
 
 function AddList() {
-    const [item, setItem] = useState("");
+    const [item, setItem] = useState({
+        companyName: "",
+        jobTitle: "",
+        workType: "",
+        pay: 0
+    });
+
+    const [err, setErr] = useState("")
+
     const {lists, fetchLists} = useContext(ListsContext);
 
     const handleInput = (e) => {
-        setItem(e.target.value)
+        setItem({...item, [e.target.name]: e.target.value})
     }
 
     const handleSubmit = e => {
+        e.preventDefault();
+        if (!item.companyName || !item.jobTitle) {
+            setErr("Do not leave anything blank.");
+            return;
+        }
         const newList = {
             id: lists.length + 1,
-            companyName: item,
-            jobTitle: "Software Engineer"
+            ...item
         }
 
         fetch("http://localhost:8000/list/", {
@@ -25,12 +37,31 @@ function AddList() {
             headers: {"Content-Type": "application/json"},
             body: JSON.stringify(newList)
         }).then(fetchLists())
+        window.location.reload();
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <input onChange={handleInput}/>
-        </form>
+        <>
+            {err && <input readOnly className="form-control-plaintext" value={err} style={{color: 'red'}}/>}
+            <form onSubmit={handleSubmit}>
+                <label className='col-sm-2 col-form-label'> Company Name
+                    <input type='text' name="companyName" value={item.companyName} onChange={handleInput}
+                           class="form-control"/>
+                </label>
+                <label> Job Title
+                    <input type='text' name="jobTitle" value={item.jobTitle} onChange={handleInput}
+                           className="form-control"/>
+                </label>
+                <label> Work Type
+                    <input type='text' name="workType" value={item.workType} onChange={handleInput}
+                           className="form-control"/>
+                </label>
+                <label> Salary
+                    <input type='number' name="pay" value={item.pay} onChange={handleInput} className="form-control"/>
+                </label>
+                <button type='submit'>Submit</button>
+            </form>
+        </>
     )
 }
 
@@ -56,11 +87,6 @@ export default function JobLists() {
                     <th>Salary</th>
                 </tr>
                 {lists.map(ls => {
-                    // const arr = []
-                    // arr.push(ls.companyName)
-                    // arr.push(ls.jobTitle)
-                    // arr.push(ls.workType)
-                    // arr.push(ls.pay)
                     const arr = Object.entries(ls).slice(1, 5)
                     return (<tr key={ls.id}>
                         {arr.map(([k, v]) => {
